2ed8410ecceb360984d8579313f84bfde721fc7a
Marten <marten.mildt@web.de>
2024-01-21 12:51:53 +0100

    Added GetTrackName. WIP MenuTrackSelection.

--- src/Functions.h
+++ src/Functions.h
@@ -2,3 +2,5 @@
 #include "Globals.h"
 
 void __cdecl HandleCircuit(UnknStruct0* pStruct);           // FUN_0043b0b0
+void MenuTrackSelection();                                  // FUN_0043b240
+const char* GetTrackName(int32_t TrackID);                  // FUN_00440620
--- src/Types.h
+++ src/Types.h
@@ -1,10 +1,27 @@
 #pragma once
 #include <stdint.h>
 
+typedef long double float10;
+
 struct UnknStruct0
 {
-    uint8_t Unkn0[94];
+    uint8_t Unkn0[0x0C];
+    int32_t Field12_0x0C;
+    uint8_t Unkn1[0x4D];
+    int8_t  TrackID;
     uint8_t CircuitIdx;
-    uint8_t Unkn1[13];
+    uint8_t Unkn2[0xD];
     uint8_t bIsTournament;
 };
+
+struct UnknStruct1
+{
+    int32_t UnknInt0;
+    int32_t UnknInt1;
+    int32_t UnknInt2;
+};
+
+static_assert(offsetof(UnknStruct0, Field12_0x0C)    == 0x0C);
+static_assert(offsetof(UnknStruct0, TrackID)         == 0x5D);
+static_assert(offsetof(UnknStruct0, CircuitIdx)      == 0x5E);
+static_assert(offsetof(UnknStruct0, bIsTournament)   == 0x6C);
