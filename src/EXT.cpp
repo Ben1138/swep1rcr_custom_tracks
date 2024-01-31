883ba893b224e02009c3741405fcbcc0fc5dd2c3
Marten <marten.mildt@web.de>
2024-01-31 10:46:18 +0100

    DrawTextBox

--- src/EXT.cpp
+++ src/EXT.cpp
@@ -1,8 +1,10 @@
 #include "EXT.h"
 #include "ReverseEngineering/Globals.h"
+#include "ReverseEngineering/FUN.h"
 #include "CustomTracks.h"
 #include <assert.h>
 #include <stdio.h>
+#include <string.h>
 
 namespace EXT
 {
@@ -59,5 +61,78 @@ namespace EXT
         // 144 slots / 2 = 72 custom tracks
         assert(TrackIdx < CustomTracks::MAX);
         return 256 + CustomTracks::MAX + TrackIdx;
+    }
+
+    void DrawTextBox(
+        uint16_t PosX, 
+        uint16_t PosY, 
+        uint8_t R, 
+        uint8_t G, 
+        uint8_t B, 
+        uint8_t A, 
+        const char* pFormatting,
+        const char* pText, 
+        uint16_t LineLengthMax, 
+        uint16_t LinesMax, 
+        uint16_t LineSpacing)
+    {
+        // Since 'UIText' expects a null terminated string
+        // I have to make a copy for each line, unfortunately...
+        constexpr uint16_t LINES_MAX = 128;
+        constexpr uint16_t LINE_LENGTH_MAX = 512;
+
+        const uint16_t FormattingLen = strnlen(pFormatting, LINE_LENGTH_MAX);
+        
+        if (LinesMax > LINES_MAX)
+        {
+            LinesMax = LINES_MAX;
+        }
+        if (LineLengthMax > LINE_LENGTH_MAX - FormattingLen - 1)
+        {
+            LineLengthMax = LINE_LENGTH_MAX - FormattingLen - 1;
+        }
+
+        char Lines[LINES_MAX][LINE_LENGTH_MAX];
+
+        uint16_t LineCount = 0;
+        const char* pLastSpace = pText;
+        const char* pLastNewLine = pText;
+        for (;LineCount < LinesMax; pText++)
+        {
+            if (*pText == ' ')
+            {
+                pLastSpace = pText + 1;
+            }
+            
+            uint16_t LineLen = pText - pLastNewLine;
+            if (LineLen >= LineLengthMax)
+            {
+                LineLen = LineLengthMax;
+            }
+
+            if (*pText == 0)
+            {
+                strncpy(Lines[LineCount], pFormatting, FormattingLen);
+                strncpy(Lines[LineCount] + FormattingLen, pLastNewLine, LineLen);
+                Lines[LineCount++][FormattingLen + LineLen] = 0;
+                break;
+            }
+
+            if (*pText == '\n' || (LineLen >= LineLengthMax && pLastSpace != pLastNewLine))
+            {
+                strncpy(Lines[LineCount], pFormatting, FormattingLen);
+                strncpy(Lines[LineCount] + FormattingLen, pLastNewLine, LineLen);
+                Lines[LineCount++][FormattingLen + LineLen] = 0;
+
+                for (; *pText == ' '; pText++);
+                pLastSpace = pText;
+                pLastNewLine = pText;
+            }
+        }
+
+        for (uint16_t i = 0; i < LineCount; i++)
+        {
+            FUN::UIText(PosX, PosY + (LineSpacing * i), R, G, B, A, Lines[i]);
+        }
     }
 }
--- src/EXT.h
+++ src/EXT.h
@@ -9,4 +9,18 @@ namespace EXT
     TrackInfo GetTrackInfo(int8_t TrackID);
     uint16_t GetImgStartBackground(uint16_t TrackIdx);
     uint16_t GetImgStartBorder(uint16_t TrackIdx);
+
+    void DrawTextBox(
+        uint16_t PosX,
+        uint16_t PosY,
+        uint8_t R,
+        uint8_t G,
+        uint8_t B,
+        uint8_t A,
+        const char* pFormatting,
+        const char* pText,
+        uint16_t LineLengthMax,
+        uint16_t LinesMax,
+        uint16_t LineSpacing
+    );
 };
