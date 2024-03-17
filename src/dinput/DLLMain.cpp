59b22e240410c8dcfe18c9e7275deabdbd92511f
Ben1138 <8263806+Ben1138@users.noreply.github.com>
2024-03-17 13:54:30 +0100

    Added mg=d5 check

--- src/dinput/DLLMain.cpp
+++ src/dinput/DLLMain.cpp
@@ -1,7 +1,9 @@
 #include <windows.h>
-#include "DynamicLoading.h"
+#include "md5.h"
 
-#if !DYN_LOAD
+#if DYN_LOAD
+#include "DynamicLoading.h"
+#else
 #include "Tracks.h"
 #include "Patching.h"
 #endif
@@ -13,7 +15,24 @@ BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpReserved)
     { 
         case DLL_PROCESS_ATTACH:
         {
-            // TODO: Check hash of EXE
+            uint8_t aMd5[16];
+            FILE* hEXE = fopen("SWEP1RCR.EXE", "rb");
+            if (!hEXE)
+            {
+                MessageBoxA(nullptr, "Failed to open 'SWEP1RCR.EXE' for verification!", "File open fail", MB_ICONERROR | MB_OK);
+                return true;
+            }
+
+            MD5::md5File(hEXE, aMd5);
+            fclose(hEXE);
+
+            uint8_t GOG_MD5[16] = { 0xE1, 0xFC, 0xF5, 0x0C, 0x8D, 0xE2, 0xDB, 0xEF, 0x70, 0xE6, 0xAD, 0x8E, 0x09, 0x37, 0x13, 0x22 };
+            if (memcmp(aMd5, GOG_MD5, 16) != 0)
+            {
+                MessageBoxA(nullptr, "This Mod is only compatible with the GOG version of the game!", "Wrong Version", MB_ICONERROR | MB_OK);
+                return true;
+            }
+
 #if DYN_LOAD
             MessageBoxA(nullptr, "dinput.dll loaded!", "DLL loaded", MB_ICONINFORMATION | MB_OK);
             DynamicLoading::Init();
